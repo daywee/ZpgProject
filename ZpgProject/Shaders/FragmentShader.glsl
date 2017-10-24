6a06bc5a1d08567ad9cac3d37c4adf1852857fe3
daywee <d4v1d.nemec@gmail.com>
2017-10-24 12:13:19 +0200

    Fix light in shader

--- ZpgProject/Shaders/FragmentShader.glsl
+++ ZpgProject/Shaders/FragmentShader.glsl
@@ -2,11 +2,12 @@
 
 in vec4 worldPosition;
 in vec3 worldNormal;
+uniform vec3 worldLightPosition;
 
 out vec4 frag_colour;
 
 void main () {
-	vec3 lightVector = normalize(vec3(0.0, 0.0, 10.0));
+	vec3 lightVector = normalize(worldLightPosition - vec3(worldPosition));
 	float dot_product = max(dot(vec4(lightVector, 1.0), normalize(vec4(worldNormal, 0.0))), 0.0);
 	vec4 diffuse = dot_product * vec4(0.385, 0.647, 0.812, 1.0);
 	vec4 ambient = vec4(0.1, 0.1, 0.1, 1.0);
--- ZpgProject/Shaders/VertexShader.glsl
+++ ZpgProject/Shaders/VertexShader.glsl
@@ -5,7 +5,6 @@ layout(location = 1) in vec3 normal;
 uniform mat4 modelMatrix;
 uniform mat4 viewMatrix;
 uniform mat4 projectionMatrix;
-uniform mat3 normalMatrix;
 
 out vec4 worldPosition;
 out vec3 worldNormal;
